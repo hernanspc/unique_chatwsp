4d7593e47a34ef0d01973cac92fa85113b4c89d9
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-07-03 18:04:06 -0500

    finish cap 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unique_chatwsp/feature/welcome/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'WhassApp Mee',
-      home: Scaffold(
-        body: Center(
-          child: Text('Demo Pagee'),
-        ),
-      ),
+      theme: ThemeData.dark(),
+      home: const WelcomePage(),
     );
   }
 }
