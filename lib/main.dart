46f0449f96c86f161db56ab3c08f5abeec139c80
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-07-05 10:22:58 -0500

    firebase add android e ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unique_chatwsp/common/theme/dark_theme.dart';
 import 'package:unique_chatwsp/common/theme/light_theme.dart';
@@ -5,8 +6,13 @@ import 'package:unique_chatwsp/feature/auth/pages/login_page.dart';
 import 'package:unique_chatwsp/feature/auth/pages/user_info_page.dart';
 import 'package:unique_chatwsp/feature/auth/pages/verification_page.dart';
 import 'package:unique_chatwsp/feature/welcome/pages/welcome_page.dart';
+import 'package:unique_chatwsp/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
